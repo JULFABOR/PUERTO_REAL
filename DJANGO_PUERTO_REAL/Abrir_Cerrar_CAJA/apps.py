868e6ab0ddc855cdb317067c6916b029c2c860dd
ViL4 <vilamaniaxel23@gmail.com>
2025-08-31 17:35:00 -0300

    modulo de ventas, falta loguin

--- DJANGO_PUERTO_REAL/Abrir_Cerrar_CAJA/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AbrirCerrarCajaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Abrir_Cerrar_CAJA'
--- DJANGO_PUERTO_REAL/Fidelizar_CLIENTES/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FidelizarClientesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Fidelizar_CLIENTES'
